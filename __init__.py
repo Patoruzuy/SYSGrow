55ff0df506fe758cadc225f1e9bf6283ea8301fa
Patoruzuy <soysebastianm@hotmail.com>
2024-05-19 16:08:41 +0100

    simplify the database

--- __init__.py
+++ __init__.py
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
 import os
-
 from flask import Flask
 
 
@@ -29,38 +27,4 @@ def create_app(test_config=None):
     @app.route('/hello')
     def hello():
         return 'Hello, World!'
-
-=======
-import os
-
-from flask import Flask
-
-
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'SYSGrow.sqlite'),
-    )
-
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
-
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
-
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
-
->>>>>>> 0f9b1bba6b66932c0aedb0dd06fae9c57eea0675
     return app
--- db.py
+++ db.py
@@ -1,8 +1,40 @@
-<<<<<<< HEAD
 import sqlite3
-
 import click
 from flask import current_app, g
+
+class Database:
+    def __init__(self,db_path):
+        self.db_path = db_path
+
+    def connect(self):
+        return sqlite3.connect(self.db_path)
+
+    def execture_query(self, query, params=()):
+        with self.connect() as conn:
+            cursor = conn.cursor()
+            cursor.execute(query, params)
+            conn.commit()
+
+    def fetch_one(self, query, params=()):
+        with self.connect() as conn:
+            cursor = conn.cursor()
+            cursor.execute(query, params)
+            return cursor.fetchone()
+        
+    def fetch_all(self, query, params=()):
+        with self.connect() as conn:
+            cursor = conn.cursor()
+            cursor.execute(query, params)
+            return cursor.fetchall()
+        
+    def save_data(self, table, criteria=None):
+        query = f"SELECT * FROM {table}"
+        if criteria:
+            placeholders = ' AND'.join([f"{key}=?" for key in criteria.keys()])
+            query += f" WHERE {placeholders}"
+            return self.fech_all(query, tuple(criteria.value()))
+        return self.fech_all(query)
+    
 
 
 def get_db():
@@ -20,27 +52,4 @@ def close_db(e=None):
     db = g.pop('db', None)
 
     if db is not None:
-=======
-import sqlite3
-
-import click
-from flask import current_app, g
-
-
-def get_db():
-    if 'db' not in g:
-        g.db = sqlite3.connect(
-            current_app.config['DATABASE'],
-            detect_types=sqlite3.PARSE_DECLTYPES
-        )
-        g.db.row_factory = sqlite3.Row
-
-    return g.db
-
-
-def close_db(e=None):
-    db = g.pop('db', None)
-
-    if db is not None:
->>>>>>> 0f9b1bba6b66932c0aedb0dd06fae9c57eea0675
         db.close()

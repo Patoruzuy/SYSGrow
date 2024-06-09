f54c6259e812a7c593d6c407a16f9c3fa06369f7
Patoruzuy <soysebastianm@hotmail.com>
2024-06-09 13:20:21 +0100

    change import of the sensor because is old

--- mySYSGrow/sensors/dht11_sensor.py
+++ mySYSGrow/sensors/dht11_sensor.py
@@ -1,4 +1,5 @@
-import Adafruit_DHT
+import adafruit_dht
+import board
 
 class DHT11Sensor:
     def __init__(self, pin):
@@ -8,8 +9,7 @@ class DHT11Sensor:
         Args:
             pin (int): GPIO pin number where the sensor is connected.
         """
-        self.sensor = Adafruit_DHT.DHT11
-        self.pin = pin
+        self.sensor = adafruit_dht.DHT11(pin)
 
     def read(self):
         """
@@ -19,14 +19,9 @@ class DHT11Sensor:
             dict: A dictionary containing 'temperature' and 'humidity'.
         """
         try:
-            humidity, temperature = Adafruit_DHT.read_retry(self.sensor, self.pin)
-            if humidity is not None and temperature is not None:
-                return {
-                    'temperature': temperature,
-                    'humidity': humidity
-                }
-            else:
-                raise ValueError("Failed to read from DHT sensor")
+            temperature = self.sensor.temperature
+            humidity = self.sensor.humidity
+            return {'temperature': temperature, 'humidity': humidity}
         except RuntimeError as e:
-            print(f"RuntimeError: {e}")
-            return None
+            print(f'Error reading sensor: {e}')
+            return {'temperature': None, 'humidity': None}
